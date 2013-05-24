219ca578beaa0bdabb31d86c661e09de1c26526a
rainkide@gmail.com <rainkide@gmail.com>
2013-05-24 09:26:26 +0800

    updat

--- main.go
+++ main.go
@@ -16,11 +16,6 @@ func main() {
 	file := path.Join(basepath, "src/configs", "app.yaml")
 	app := dogo.NewApp(file)
 
-	//register controller
-	app.RegisterCtrl("admin", &admin.Errors{})
-	app.RegisterCtrl("admin", &admin.Login{})
-	app.RegisterCtrl("admin", &admin.Index{})
-
 	//bootstart and run
 	app.Bootstrap(router).SetDefaultModule("admin").Run()
 }
@@ -32,6 +27,11 @@ func getRouter() *dogo.Router {
 
 	//add static router
 	router.AddStaticRoute("/statics", path.Join(basepath, "src/statics/"))
+
+	//add sample route
+	router.AddSampleRoute("admin", &admin.Errors{})
+	router.AddSampleRoute("admin", &admin.Login{})
+	router.AddSampleRoute("admin", &admin.Index{})
 
 	//add regex router and default is sample reoute
 	router.AddRegexRoute("/login", "/admin/login/index")
